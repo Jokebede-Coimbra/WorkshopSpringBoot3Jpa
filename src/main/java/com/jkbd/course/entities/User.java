3b9248f9148100aeb969596a3dc5dcfb89bd3ae9
Coimbra <adsjkbd@gmail.com>
2024-08-26 07:43:11 -0300

    H2 database, test profile, JPA

--- src/main/java/com/jkbd/course/entities/User.java
+++ src/main/java/com/jkbd/course/entities/User.java
@@ -1,18 +1,21 @@
 package com.jkbd.course.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
 
-
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
