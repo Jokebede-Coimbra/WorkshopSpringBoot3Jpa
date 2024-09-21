37845cc9a5068235a2f26a7823b72407357b5320
Coimbra <adsjkbd@gmail.com>
2024-09-21 09:34:41 -0300

    melhorando o método getTotal

--- src/main/java/com/jkbd/course/entities/Order.java
+++ src/main/java/com/jkbd/course/entities/Order.java
@@ -86,11 +86,9 @@ public class Order implements Serializable {
     }
 
     public Double getTotal() {
-        double sum = 0;
-        for (OrderItem x : items) {
-            sum += x.getSubTotal();
-        }
-        return sum;
+        return items.stream()
+                .mapToDouble(OrderItem::getSubTotal)
+                .sum();
     }
 
     @Override
