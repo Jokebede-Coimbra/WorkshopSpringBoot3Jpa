7f916a2d11850105e1ba3900afc253d740194b8a
Coimbra <adsjkbd@gmail.com>
2024-09-25 06:09:00 -0300

    Exception handling - findById

--- src/main/java/com/jkbd/course/entities/Order.java
+++ src/main/java/com/jkbd/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.jkbd.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jkbd.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
@@ -15,6 +16,7 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
     private Integer orderStatus;
